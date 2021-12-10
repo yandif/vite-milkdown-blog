e9d90d4902e3ea459b65c6405aed2b5f595cbdd9
yandif <me.yandif@gmail.com>
2021-12-10 15:11:51 +0800

    添加antd

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import ReactDOM from 'react-dom';
-import '@/assets/style/main.less';
-import App from './App';
 import MobxProvider from '@/store';
+import { ConfigProvider } from 'antd';
+import zh_CN from 'antd/lib/locale-provider/zh_CN';
+
 import MessageProvider from '@/components/Message';
+import App from './App';
+
+import 'antd/dist/antd.css';
+import '@/assets/style/main.less';
 
 ReactDOM.render(
   <MobxProvider>
     <MessageProvider>
-      <App />
+      <ConfigProvider locale={zh_CN}>
+        <App />
+      </ConfigProvider>
     </MessageProvider>
   </MobxProvider>,
   document.getElementById('root')
